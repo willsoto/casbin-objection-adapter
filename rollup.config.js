9c5737986633816f7ebe915e9c1edfdac349e7d1
Will Soto <willsoto@users.noreply.github.com>
2020-05-10 15:02:20 -0400

    chore(rollup): remove console.log
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- rollup.config.js
+++ rollup.config.js
@@ -11,8 +11,6 @@ const globals = Object.keys(pkg.peerDependencies).reduce((obj, dependency) => {
 }, {});
 const [, pkgName] = pkg.name.split("/");
 
-console.log(globals);
-
 export default {
   input: "src/index.ts",
   output: formats.map((format) => ({
